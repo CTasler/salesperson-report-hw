fe18f76f0fecc7563e364b31f7c49dc91d454580
Claire Tasler <tasl0368@gmail.com>
2022-10-04 18:23:25 -0500

    deleted comments

--- sales_report.py
+++ sales_report.py
@@ -2,7 +2,6 @@
 
 import sys 
 melon_sales = {}
-# with open(sys.argv[1]) as file:
 file = open(sys.argv[1])
 
 def make_sales_record(file):
@@ -20,7 +19,6 @@ def print_sales_record(melon_sales):
         for key, value in melon_sales.items():
             print(f"{key} sold {value} melons.")
 
-    # for i in range(len(salespeople)):
-    #     print(f'{salespeople[i]} sold {melons_sold[i]} melons')
+
 print_sales_record(make_sales_record(file))
 file.close()
